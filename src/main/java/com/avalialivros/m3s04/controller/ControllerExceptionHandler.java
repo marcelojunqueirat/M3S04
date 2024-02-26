30e94ad75b5aa76c0395cfc5fb63048d970a1d6d
Marcelo Junqueira Teixeira <marcelojxt@gmail.com>
2024-02-26 13:43:14 -0300

    [M03S04] Mini-Projeto Avaliações - Busca por um livro - Implementando exception em controller handler

--- src/main/java/com/avalialivros/m3s04/controller/ControllerExceptionHandler.java
+++ src/main/java/com/avalialivros/m3s04/controller/ControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.avalialivros.m3s04.controller;
 
+import com.avalialivros.m3s04.exceptions.BookNotFoundException;
 import com.avalialivros.m3s04.exceptions.PersonNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
@@ -12,6 +13,12 @@ public class ControllerExceptionHandler {
 
     @ExceptionHandler(PersonNotFoundException.class)
     public ResponseEntity<ProblemDetail> handlePersonNotFoundException(PersonNotFoundException exception) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, exception.getMessage());
+        return ResponseEntity.badRequest().body(problemDetail);
+    }
+
+    @ExceptionHandler(BookNotFoundException.class)
+    public ResponseEntity<ProblemDetail> handlePersonNotFoundException(BookNotFoundException exception) {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, exception.getMessage());
         return ResponseEntity.badRequest().body(problemDetail);
     }
